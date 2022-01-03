76861b66b92fb04792336335abd277ec52a6ea37
Cyrill <cyrillkuettel@gmail.com>
2022-01-03 22:07:17 +0100

    adding conda installation script to .sh

--- post-installation-mxLinux.sh
+++ post-installation-mxLinux.sh
@@ -51,3 +51,10 @@ sudo dpkg -i lsd_0.20.1_amd64.deb
 
 sudo apt install vim
 
+# install conda
+cd
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
