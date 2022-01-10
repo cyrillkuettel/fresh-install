0cd8d1fb182fc888ff652939c6fe67749e925b42
Cyrill <cyrillkuettel@gmail.com>
2022-01-10 23:43:55 +0100

    docker-compose

--- post-installation-mxLinux.sh
+++ post-installation-mxLinux.sh
@@ -30,6 +30,9 @@ sudo apt install docker-ce
 sudo docker run hello-world
 sudo groupadd docker
 sudo usermod -aG docker $USER
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 mkdir ~/.ssh
 cd ~/.ssh
