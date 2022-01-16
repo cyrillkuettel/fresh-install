81f6853b3f52ec9a17b0f111d0596025ccd3ab8a
Cyrill <cyrillkuettel@gmail.com>
2022-01-16 01:57:42 +0100

    correcting some stuff

--- post-installation-mxLinux.sh
+++ post-installation-mxLinux.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+sudo apt update
 sudo setxkbmap ch
-
-wget https://drive.switch.ch/index.php/s/nz0UdWDlSQ9ogDX/download?path=%2F01_jdk_11.0&files=jdk-11.0.13_linux-x64_bin.tar.gz 
-tar -xzf jdk-11.0.13_linux-x64_bin.tar.gz
-sudo mv jdk-11.0.13 /usr/lib/jvm
+cd ~/Downloads
+wget https://drive.switch.ch/index.php/s/nz0UdWDlSQ9ogDX/download?path=%2F01_jdk_11.0&files=jdk-11.0.13_linux-x64_bin.tar.gz -O jdk.tar.gz
+tar -xzf jdk.tar.gz
+Dir_name=`tar -tzf jdk.tar.gz | head -1 | cut -f1 -d"/"`
+echo $Dir_name
+sudo mv $Dir_name /usr/lib/jvm
 
 
 # TODO: Add JAVA_HOME stuff: copy from my computer
@@ -14,7 +17,7 @@ wget https://drive.switch.ch/index.php/s/nz0UdWDlSQ9ogDX/download?path=%2F10_mav
 
 
 # Pycharm
-wget -O pycharm.tar.gz https://drive.switch.ch/index.php/s/nz0UdWDlSQ9ogDX/download?path=%2F73_pycharm&files=pycharm-community-2021.2.3.tar.gz
+wget https://drive.switch.ch/index.php/s/nz0UdWDlSQ9ogDX/download?path=%2F73_pycharm&files=pycharm-community-2021.2.3.tar.gz -O pycharm.tar.gz
 tar -xzf pycharm.tar.gz
 sudo mv pycharm-2021.3  /opt/
 bash /opt/pycharm-2021.3/bin/pycharm.sh
@@ -25,7 +28,6 @@ gpg   --keyid-format 0xlong docker.gpg 2>/dev/null
 gpg  --keyid-format 0xlong docker.gpg 2>/dev/null | grep -sq 0x8D81803C0EBFCD88 && sudo apt-key add docker.gpg
 CODENAME=buster
 echo "deb [arch=amd64] https://download.docker.com/linux/debian $CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt update
 sudo apt install docker-ce
 sudo docker run hello-world
 sudo groupadd docker
@@ -37,9 +39,9 @@ sudo chmod +x /usr/local/bin/docker-compose
 mkdir ~/.ssh
 cd ~/.ssh
 ssh-keygen -t ed25519 -C "cyrillkuettel@gmail.com"
-ssh-add /home/demo/.ssh/id_ed25519
+ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
-# Add the key to github programmatically
+# TODO: Add the key to github programmatically
 
 git config --global user.email "cyrillkuettel@gmail.com"
 git config --global user.name "Cyrill"
@@ -55,12 +57,25 @@ sudo dpkg -i lsd_0.20.1_amd64.deb
 sudo apt install vim
 sudo apt install ffmpeg x264 x265 # this is used for example so that youtube-dl can produce mp3s
 # install conda
-cd
+cd ~/
 mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 
 # if you want to debug native process in android studio. If this is not installed it will result in a crash when running the debugger on a ndk project. 
 sudo apt install libncurses5
+
+# git large file storage
+cd ~/Downloads
+wget https://github.com/git-lfs/git-lfs/releases/download/v3.0.2/git-lfs-linux-amd64-v3.0.2.tar.gz -O git-lfs.tar.gz
+mkdir git-lfs
+tar -xzf git-lfs.tar.gz -C ./git-lfs
+
+
+
+
+
+
+
