4f28ac428446dfac0d08cec93c55618567aeadcf
Cyrill <cyrillkuettel@gmail.com>
2022-09-25 02:50:06 +0200

    various vim config

---
+++ vim/install.sh
@@ -0,0 +1,12 @@
+# https://github.com/junegunn/vim-plug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\n    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# 1.) add the stuff to vimrc
+# 2.) run this:
+
+
+# :PluginInstall
+
+# possible to also run from terminal:
+vim +PluginUpdate +qall
