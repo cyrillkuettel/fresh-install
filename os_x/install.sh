d86a6e353f8db8e283ac44106a3d5b15110e7623
cyrillkuettel <cyrillkuettel@gmail.com>
2023-03-12 18:46:00 +0100

    Basic utils on OS X

---
+++ os_x/install.sh
@@ -0,0 +1,12 @@
+# Core 
+  brew update
+  brew install ripgrep
+  brew install wget
+  brew install coreutils
+  brew install --cask htop
+  brew install htop
+  brew install neofetch
+  brew install nvm 
+  brew install java
+  brew install clang-format
+  brew install vlc
---
+++ os_x/shorcuts.md
@@ -0,0 +1,8 @@
+XCode
+=======
+Go to definition: CONTROL + COMMAND + J
+Format code: COMMAND + A, then CONTROL + I to format all selection
+GENERAL
+=====
+Open new window: COMMAND + N
+Minimize window: COMMAND + M
