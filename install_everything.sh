b1ac63f312419eab04f074afed7bc7fae4e6ab4c
cyrillkuettel <cyrillkuettel@gmail.com>
2023-04-11 18:57:07 +0200

    Adds software

--- install_everything.sh
+++ install_everything.sh
@@ -12,6 +12,7 @@ sudo apt update
 sudo apt install ffmpeg x264 x265 
 
 sudo apt install -y htop ripgrep xsel keepassxc build-essential curl file xclip
+vlc 
 python3 python3.10-venv terminator fd-find
 
 ln -s $(which fdfind) ~/.local/bin/fd
