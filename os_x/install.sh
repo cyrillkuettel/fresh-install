2835daa537b5cd5fe41b813bfc0e1b0806409388
cyrillkuettel <cyrillkuettel@gmail.com>
2023-07-08 02:24:41 +0200

    Add os x instructions for pdf generating

--- os_x/install.sh
+++ os_x/install.sh
@@ -11,3 +11,19 @@
   brew install clang-format
   brew install vlc
   brew install --cask calibre
+
+  # generate pdf from markdown 
+  brew tap homebrew/cask-fonts
+  brew install --cask font-open-sans
+  brew install --cask basictex
+   
+   # get the font
+    curl https://fontlibrary.org/assets/downloads/eb-garamond/ad76a14ea9b136557b474c3445caea08/eb-garamond.zip
+  # unzip, put contents of tff folder accordingly:
+  sudo cp *.ttf /Library/Fonts/
+
+  eval "$(/usr/libexec/path_helper)"
+
+
+
+
