69a0430b547a80214d939fefd96f52e35b37fde5
cyrillkuettel <cyrillkuettel@gmail.com>
2023-08-16 00:21:56 +0200

    Turn off annoying macOS 'Search man Page Index in Terminal' shortcut via the command line that conflict with one of Pycharm's best shortcuts (Find Action)

--- mac_os/install.sh
+++ mac_os/install.sh
@@ -70,3 +70,37 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 # Rosetta for x86_64. For example, building PyQt5 is impossible without this.
 /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 
+
+
+# Turn off annoying macOS man page shortcuts via the command line
+# that conflict with one of IntelliJ IDEA’s best shortcuts (Find Action)
+# from here:
+# https://gist.github.com/theodson/b4282a3b6e54091db4d52a4c3c10ad25
+
+TEMP_SETTINGS_FILE=$(mktemp -t 'man-shortcuts-off.json')
+cat > $TEMP_SETTINGS_FILE <<EOF
+{
+  "NSServicesStatus": {
+    "com.apple.Terminal - Open man Page in Terminal - openManPage": {
+      "presentation_modes": {
+        "ContextMenu": false,
+        "ServicesMenu": false
+      },
+      "enabled_context_menu": false,
+      "enabled_services_menu": false
+    },
+    "com.apple.Terminal - Search man Page Index in Terminal - searchManPages": {
+      "presentation_modes": {
+        "ContextMenu": false,
+        "ServicesMenu": false
+      },
+      "enabled_context_menu": false,
+      "enabled_services_menu": false
+    }
+  }
+}
+EOF
+# Settings are stored in the pbs domain. Other settings in this domain will not be overwritten. I’ve included the settings to change in JSON for brevity. They are converted to XML (which `defaults import` expects) before being imported.
+plutil -convert xml1 -o - ${TEMP_SETTINGS_FILE} | defaults import pbs -
+rm ${TEMP_SETTINGS_FILE}
+
