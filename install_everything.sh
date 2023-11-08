56688b5385b92edc4ad25dbd49372ec418747352
cyrillkuettel <cyrillkuettel@gmail.com>
2023-11-08 21:09:04 +0100

    remove kitty

--- install_everything.sh
+++ install_everything.sh
@@ -101,26 +101,5 @@ source ~/.zshrc
 pip install 'black[d]'
 
 
-# kitty
-cp ~/fresh-install/kitty.conf ~/.config/kitty/kitty.conf
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh --output installer.sh
-chmod +x installer.sh
-./installer.sh
-# Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
-# your system-wide PATH)
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-# Place the kitty.desktop file somewhere it can be found by the OS
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-# Update the paths to the kitty and its icon in the kitty.desktop file(s)
-sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-
-# set as default
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator `which kitty` 50
-sudo update-alternatives --config x-terminal-emulator
-
-
 mkdir -p ~/.local/bin/ && cp ~/fresh-install/shell/rfv ~/.local/bin
 
