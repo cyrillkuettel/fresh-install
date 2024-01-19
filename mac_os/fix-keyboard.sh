28ff2284db700ae627df762670b753fdcad9e42e
cyrillkuettel <cyrillkuettel@gmail.com>
2024-01-19 15:05:37 +0100

    MacOS iso keyboard issue

--- mac_os/fix-keyboard.sh
+++ mac_os/fix-keyboard.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo mv mac_os/brackets_like_swiss_layout.keylayout /Library/Keyboard\ Layouts
-sudo touch /Library/Keyboard\ Layouts
+# anyoying issue where the key '><' is swapped with '§°' on german layout
+
+sudo cp mac_os/brackets_like_swiss_layout.keylayout /Library/Keyboard\ Layouts
+sudo rm /Library/Preferences/com.apple.keyboardtype.plist ~/Desktop # I think this one did it
+sudo reboot
+
+# sudo touch /Library/Keyboard\ Layouts
+
