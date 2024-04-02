3f475598ca0a506b9974b604d81aa3e72fb12a85
cyrillkuettel <cyrillkuettel@gmail.com>
2024-04-02 16:07:58 +0200

    fix typo

--- mac_os/fix-keyboard.sh
+++ mac_os/fix-keyboard.sh
@@ -3,7 +3,7 @@
 # anyoying issue where the key '><' is swapped with '§°' on german layout
 
 sudo cp mac_os/brackets_like_swiss_layout.keylayout /Library/Keyboard\ Layouts
-sudo rm /Library/Preferences/com.apple.keyboardtype.plist ~/Desktop # I think this one did it
+sudo rm /Library/Preferences/com.apple.keyboardtype.plist # I think this one did it
 sudo reboot
 
 # sudo touch /Library/Keyboard\ Layouts
