7678eaafc5bba681fcde8e603fcc461e1b07a295
Cyrill <cyrillkuettel@gmail.com>
2025-03-22 14:41:55 +0100

    Add desktop icon for firefox so it appears in rofi.

--- firefox/install_firefox.sh
+++ firefox/install_firefox.sh
@@ -23,4 +23,29 @@ sudo mv firefox "$INSTALL_DIR"
 # Create a symbolic link
 sudo ln -sf "$INSTALL_DIR/firefox" /usr/bin/firefox
 
-echo "Firefox installation completed."
+DESKTOP_FILE="/usr/share/applications/firefox-developer.desktop"
+# Check if desktop entry exists and create if it doesn't
+if [ ! -f "$DESKTOP_FILE" ]; then
+    echo "Creating desktop entry file..."
+    sudo tee "$DESKTOP_FILE" > /dev/null << EOL
+[Desktop Entry]
+Name=Firefox Developer Edition
+GenericName=Web Browser
+Comment=Browse the Web
+Exec=/opt/firefox/firefox %u
+Terminal=false
+Type=Application
+Icon=/opt/firefox/browser/chrome/icons/default/default128.png
+Categories=Network;WebBrowser;
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;
+StartupNotify=true
+EOL
+    echo "Desktop entry file created."
+else
+    echo "Desktop entry file already exists."
+fi
+
+# Update desktop database
+sudo update-desktop-database
+
+echo "Firefox Developer Edition installation completed and added to application menu."
